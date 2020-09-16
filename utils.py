f290f6c6fc7df3da0ada0d6b84d2021be0e4caab
kshth12 <59958455+kshth12@users.noreply.github.com>
2020-09-17 02:04:24 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -48,16 +48,26 @@ def getPrice(ticker):
 
     return TICKER_CACHE[ticker]
 
-
 def topthreecards():
     stocks = session.query(CurrentPortfolio).filter(CurrentPortfolio.typeOfInstrument == 'STOCK')
     total_stock_valuation = 0
     total_investment = 0
+    max_unrealized_pl = 0
+    max_gainer = ''
+    min_unrealized_pl = 0
+    max_loser = ''
     for row in stocks:
         current_market_price = (getPrice(str(row.ticker)))
         quantity = int(row.quantity)
         total_stock_valuation = total_stock_valuation+(current_market_price*quantity)
         total_investment = total_investment+(row.averagePrice*quantity)
+        unrealized_pl = total_stock_valuation-total_investment
+        if unrealized_pl>max_unrealized_pl:
+            max_unrealized_pl = unrealized_pl
+            max_gainer = session.query(StockDescrp.stockName).filter(StockDescrp.isin==row.isin).one()[0]
+        if unrealized_pl<min_unrealized_pl:
+            min_unrealized_pl = unrealized_pl
+            max_loser = session.query(StockDescrp.stockName).filter(StockDescrp.isin==row.isin).one()[0]
     bonds = session.query(CurrentPortfolio).filter(CurrentPortfolio.typeOfInstrument == 'BOND')
     total_bond_valuation = 0
     for row in bonds:
@@ -65,8 +75,17 @@ def topthreecards():
         quantity = int(row.quantity)
         total_bond_valuation = total_bond_valuation+current_market_price*quantity
         total_investment = total_investment+(row.averagePrice*quantity)
+        unrealized_pl = total_bond_valuation-total_investment
+        if unrealized_pl>max_unrealized_pl:
+            max_unrealized_pl = unrealized_pl
+            max_gainer = session.query(BondDescrp.bondName).filter(BondDescrp.isin==row.isin).one()[0]
+        if unrealized_pl<min_unrealized_pl:
+            min_unrealized_pl = unrealized_pl
+            max_loser = session.query(BondDescrp.bondName).filter(BondDescrp.isin==row.isin).one()[0]
     total_valuation = total_stock_valuation+total_bond_valuation
-    res = {'field1': "My investment",'value1': total_investment, 'field2' :"My networth",'value2':total_valuation}
+    total_unrealized_pl = total_valuation-total_investment
+    total_pl_percentage = (total_unrealized_pl/total_investment)*100
+    res = {'card1':{'field1': 'My investment','value1': total_investment, 'field2' :'My networth','value2':total_valuation},'card2':{'field1': 'Total Profit&Loss','value1': total_unrealized_pl,'field2': 'Total Profit&Loss%','value2': total_pl_percentage},'card3':{'field1': 'Max Gainer','value1': max_gainer,'field2': 'Max Loser','value2': max_loser}}
     return (res)
 
 
@@ -137,6 +156,7 @@ def tabledata():
     total_stock_valuation = 0
     total_stock_investment = 0
     stock_details_data = []
+    total_stock_unrealized_pl = 0
     for row in stocks:
         current_market_price = (getPrice(str(row.ticker)))
         quantity = int(row.quantity)
@@ -145,13 +165,14 @@ def tabledata():
         current_investment = row.averagePrice*quantity
         total_stock_investment = total_stock_investment+current_investment
         unrealized_PL = current_valuation-current_investment
+        total_stock_unrealized_pl = total_stock_unrealized_pl+unrealized_PL
         stockName = session.query(StockDescrp.stockName).filter(StockDescrp.isin==row.isin).one()[0]
         stock_details_data.append({'name': stockName, 'quantity': quantity, 'investment': current_investment,'currentValuation': current_valuation, 'unrealizedPL': unrealized_PL })
-    stock_summary = {'investment': total_stock_investment,'currentValuation': total_stock_valuation, 'unrealizedPL': total_stock_valuation-total_stock_investment }
     bonds = session.query(CurrentPortfolio).filter(CurrentPortfolio.typeOfInstrument == 'BOND')
     total_bond_valuation = 0
     total_bond_investment = 0
     bond_details_data = []
+    total_bond_unrealized_pl = 0
     for row in bonds:
         current_market_price = row.averagePrice+random.randrange(-10, 10, 1)
         quantity = int(row.quantity)
@@ -160,14 +181,25 @@ def tabledata():
         current_investment = row.averagePrice*quantity
         total_bond_investment = total_bond_investment+current_investment
         unrealized_PL = current_valuation-current_investment
+        total_bond_unrealized_pl = total_bond_unrealized_pl+unrealized_PL
         bondName = session.query(BondDescrp.bondName).filter(BondDescrp.isin==row.isin).one()[0]
         bond_details_data.append({'name': bondName, 'quantity': quantity,'investment':current_investment, 'currentValuation':current_valuation ,'unrealizedPL': unrealized_PL })
-    bond_summary = {'investment': total_bond_investment,'currentValuation': total_bond_valuation, 'unrealizedPL': total_bond_valuation-total_bond_investment }
+    total_unrealized_pl = total_bond_unrealized_pl+total_stock_unrealized_pl
+    total_valuation = total_stock_valuation+total_bond_valuation
+    stock_valuation_percentage = (total_stock_valuation/total_valuation)*100
+    stock_unrealized_pl_percentage = (total_stock_unrealized_pl/total_unrealized_pl)*100
+    bond_valuation_percentage = (total_bond_valuation/total_valuation)*100
+    bond_unrealized_pl_percentage = (total_bond_unrealized_pl/total_unrealized_pl)*100
+    stock_summary = {'investment': total_stock_investment,'currentValuation': total_stock_valuation, 'currentValuationPercentage': stock_valuation_percentage,'unrealizedPL': total_stock_unrealized_pl, 'unrealizedPLPercentage': stock_unrealized_pl_percentage }
+    bond_summary = {'investment': total_bond_investment,'currentValuation': total_bond_valuation, 'currentValuationPercentage': bond_valuation_percentage, 'unrealizedPL': total_bond_unrealized_pl, 'unrealizedPLPercentage': bond_unrealized_pl_percentage }
+    for i in range(0,len(stock_details_data)):
+        stock_details_data[i]['unrealizedPLPercentage'] = (stock_details_data[i]['unrealizedPL']/total_unrealized_pl)*100
+        stock_details_data[i]['currentValuationPercentage'] = (stock_details_data[i]['currentValuation']/total_valuation)*100
+    for i in range(0,len(bond_details_data)):
+        bond_details_data[i]['unrealizedPLPercentage'] = (bond_details_data[i]['unrealizedPL']/total_unrealized_pl)*100
+        bond_details_data[i]['currentValuationPercentage'] = (bond_details_data[i]['currentValuation']/total_valuation)*100
     res = [{'securityType': 'Equity', 'summaryData': stock_summary , 'detailsData' : stock_details_data},{'securityType': 'Bonds', 'summaryData': bond_summary, 'detailsData' : bond_details_data}]
     return ( res )
-
-
-
 
 # NEHAL will do the changes here
 def clienthistory():
